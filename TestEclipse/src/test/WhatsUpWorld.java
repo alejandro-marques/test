77fba3a53195ec8db9f21acd1c3c25a8bba75ed4
amarques <amarques@stratio.com>
2015-06-23 17:42:41 +0200

    Clean up HelloWorld

---
+++ TestEclipse/src/test/WhatsUpWorld.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by alejandro on 23/06/15.
+ */
+public class WhatsUpWorld {
+    public static void main(String[] args) {
+        System.out.println("What's up, Wolrd?");
+    }
+}
