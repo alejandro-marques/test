f6c0b47981c1307c2b37c3a6d440b865c5bf3405
amarques <amarques@stratio.com>
2015-06-23 17:30:57 +0200

    Changed readme and created ByeWorld

---
+++ TestEclipse/src/test/ByeWorld.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by alejandro on 23/06/15.
+ */
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Bye, World!");
+    }
+}
