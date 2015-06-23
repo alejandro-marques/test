73b156fe98c3b8a0557d15097f004ede6137e69f
amarques <amarques@stratio.com>
2015-06-23 17:35:04 +0200

    Added line

--- TestEclipse/src/test/HelloWorld.java
+++ TestEclipse/src/test/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     // Comment
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("What's up?");
         System.out.println("Bye, World!");
         System.out.println("END");
     }
