ef480e516a212e0dfa1aee9d4a2d4de84bc4b595
amarques <amarques@stratio.com>
2015-06-23 17:40:39 +0200

    Clean up HelloWorld

--- TestEclipse/src/test/HelloWorld.java
+++ TestEclipse/src/test/HelloWorld.java
@@ -5,7 +5,5 @@ public class HelloWorld {
     // Comment
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("What's up?");
-        System.out.println("END");
     }
 }
