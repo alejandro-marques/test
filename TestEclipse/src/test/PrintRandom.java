0ce748d279b129ba6e2d7f9657f68bb5efe9ebf6
amarques <amarques@stratio.com>
2015-06-25 10:00:13 +0200

    Added comment

--- TestEclipse/src/test/PrintRandom.java
+++ TestEclipse/src/test/PrintRandom.java
@@ -7,6 +7,7 @@ public class PrintRandom {
         // Prints a random number
         System.out.println("Random number: " + new Random().nextInt());
 
+        // Prints random number from 0 to 10
         System.out.println("Random number [0-10]: " + new Random().nextInt(11));
     }
 }
