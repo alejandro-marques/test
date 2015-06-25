b5cfeae62d4f58aa110cbfd505b51eb6ebebfcd2
amarques <amarques@stratio.com>
2015-06-25 09:59:38 +0200

    Added new random

--- TestEclipse/src/test/PrintRandom.java
+++ TestEclipse/src/test/PrintRandom.java
@@ -6,5 +6,7 @@ public class PrintRandom {
     public static void main(String[] args) {
         // Prints a random number
         System.out.println("Random number: " + new Random().nextInt());
+
+        System.out.println("Random number [0-10]: " + new Random().nextInt(11));
     }
 }
