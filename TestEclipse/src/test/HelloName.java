f224ec6d88c81824f01475b696785d22abe949c4
amarques <amarques@stratio.com>
2015-06-25 09:34:02 +0200

    Added age control

--- TestEclipse/src/test/HelloName.java
+++ TestEclipse/src/test/HelloName.java
@@ -9,8 +9,18 @@ public class HelloName {
     public static void main(String[] args) {
 
         Scanner keyboard = new Scanner(System.in);
+
         System.out.println("Hello, what's your name?");
         String name = keyboard.nextLine();
         System.out.println("Nice to meet you " + name);
+
+        System.out.println("How old are you?");
+        int age = keyboard.nextInt();
+        if (age < 18){
+            System.out.println("You must be over 18 to talk with me");
+        }
+        else {
+            System.out.println("Speak friend and enter.");
+        }
     }
 }
