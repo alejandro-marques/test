dea4bbb7e0f3526254b56bf246b1ca47ab2edc98
amarques <amarques@stratio.com>
2015-06-25 09:51:54 +0200

    Prints random number

--- TestEclipse/src/test/PrintRandom.java
+++ TestEclipse/src/test/PrintRandom.java
@@ -4,6 +4,6 @@ import java.util.Random;
 
 public class PrintRandom {
     public static void main(String[] args) {
-        System.out.println(new Random().nextInt());
+        System.out.println("Random number: " + new Random().nextInt());
     }
 }
